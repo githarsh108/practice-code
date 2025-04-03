60e98151bf0f4699b6c70b9694f4006fc2f1994b
Harsh Gupta <137254751+githarsh108@users.noreply.github.com>
2025-04-03 22:07:23 +0000

    commiting a new change

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accName = "Rohit Sharma"
+let hisId = "1794"
+var state = "delhi"
+
+hisId = "1888"
+state="jaipur"
+
+
+/*
+prefer not to use var in javascript  as it creates issues in block scope
+*/ 
+
+console.table([accName, hisId, state])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"  //treats all javascripts code as the newer versions of code
+
+// alert("hello") // it can't be runned on node js environment only be runned on front end or console
+
+
+let name = "harsh"
+let value = 18
+let isLoggedIn = true;
+
+/*data types -- 
+
+number
+bigint
+string => ""
+boolean => 0/1
+null => standalone value
+undefined =>
+symbol => unique
+
+*/
+console.log("more changes to be done")
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
